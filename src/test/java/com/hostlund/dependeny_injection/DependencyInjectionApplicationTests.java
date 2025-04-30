edc3ee421bdc81ca5e19b403d3f7bcd9de0cd817
Oskar Höstlund <oskarhostlund@gmail.com>
2025-04-30 12:13:40 +0200

    Looking at the applciation context in the tests and using @Autowired

--- src/test/java/com/hostlund/dependeny_injection/DependencyInjectionApplicationTests.java
+++ src/test/java/com/hostlund/dependeny_injection/DependencyInjectionApplicationTests.java
@@ -1,10 +1,32 @@
 package com.hostlund.dependeny_injection;
 
+import com.hostlund.dependeny_injection.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DependencyInjectionApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testUsingTheInjectedController(){
+		String hello = myController.sayHello();
+		assert("Hello".equals(hello));
+	}
+
+	@Test
+	void testGetControllerFromContext(){
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+
+	}
 
 	@Test
 	void contextLoads() {
